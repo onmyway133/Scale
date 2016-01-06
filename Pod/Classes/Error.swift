c6ffc317e421af350ddf356cd7030ca9ee2f1cb9
Khoa Pham <onmyway133@gmail.com>
2016-01-06 18:13:39 +0700

    Add Length

---
+++ Pod/Classes/Error.swift
@@ -0,0 +1,13 @@
+//
+//  Error.swift
+//  Pods
+//
+//  Created by Khoa Pham on 1/6/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import Foundation
+
+enum Error: ErrorType {
+    case DividedByZero
+}
