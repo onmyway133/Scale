8fe086dab927e8fd4f339b0d6bf56f1a3687e923
Khoa Pham <onmyway133@gmail.com>
2016-01-06 23:26:03 +0700

    Parse Definition

--- Pod/Classes/Script/Script.swift
+++ Pod/Classes/Script/Script.swift
@@ -19,7 +19,7 @@ Declare #!/usr/bin/env xcrun swift -i at the top of the file
 
 import Foundation
 
-// MARK: Path
+// MARK: NSURL
 extension NSURL {
     func parentDirectory() -> NSURL {
         return self.URLByDeletingLastPathComponent!
@@ -28,8 +28,42 @@ extension NSURL {
     func append(pathComponent: String) -> NSURL {
         return self.URLByAppendingPathComponent(pathComponent)
     }
+
+    var correctPath: String {
+        return path ?? ""
+    }
 }
 
+// MARK: String
+extension String {
+    func substring(range: NSRange) -> String {
+        return (self as NSString).substringWithRange(range)
+    }
+
+    func split(separator: String) -> [String] {
+        return (self as NSString).componentsSeparatedByString(separator)
+    }
+
+    func remove(string: String) -> String {
+        return (self as NSString).stringByReplacingOccurrencesOfString(string, withString: "")
+    }
+}
+
+// MARK: NSRegularExpression
+extension NSRegularExpression {
+    class func find(pattern: String, string: String) throws -> [String] {
+        let regex = try NSRegularExpression(pattern: pattern, options: [.CaseInsensitive])
+        let results = regex.matchesInString(string, options: [], range: NSMakeRange(0, string.characters.count))
+
+        return results.flatMap { result in
+                    return result.range
+                }.flatMap { range in
+                    return string.substring(range)
+                }
+    }
+}
+
+// MARK: Path
 func currentDirectory() -> NSURL {
     return NSURL(fileURLWithPath: NSFileManager.defaultManager().currentDirectoryPath)
 }
@@ -38,8 +72,8 @@ func definitionPath() -> NSURL {
     return currentDirectory().parentDirectory().append("Definitions")
 }
 
-func defFilePaths() throws -> [String] {
-    return try NSFileManager.defaultManager().contentsOfDirectoryAtPath(definitionPath().absoluteString).filter {
+func defFileNames() throws -> [String] {
+    return try NSFileManager.defaultManager().contentsOfDirectoryAtPath(definitionPath().correctPath).filter {
         return $0.containsString(".def")
     }
 }
@@ -57,7 +91,8 @@ func write(filePath: String, content: String) throws {
 // DATA
 class Definition {
     var name = ""
-    var units = [String]()
+    var units = [String: String]()
+    var defaultScale = ""
 
     init() {
 
@@ -70,24 +105,56 @@ class Definition {
 
 // ACTION
 func action() throws {
-    let templatePath = definitionPath().append("Template.t").absoluteString
+    let templatePath = definitionPath().append("Template.t").correctPath
     let template = try read(templatePath)
 
     let outputPath = currentDirectory().parentDirectory().append("Output")
 
-    for defPath in try defFilePaths() {
+    for defFileName in try defFileNames() {
+        let defPath = definitionPath().append(defFileName).correctPath
+
         let def = try read(defPath)
-        let definition = parse(def)
+        let definition = try parse(def)
 
         let result = apply(template, definition: definition)
         let resultPath = outputPath.append(definition.name + ".swift")
 
-        try write(resultPath.absoluteString, content: result)
+        try write(resultPath.correctPath, content: result)
     }
 }
 
-func parse(def: String) -> Definition {
+// MARK: Parse
+func parse(def: String) throws -> Definition {
     let definition = Definition()
+
+    definition.name = try {
+        guard let match = try NSRegularExpression.find("enum\\s\\w+Unit", string: def).first else {
+            return ""
+        }
+
+        return match.remove("Unit").split(" ").last ?? ""
+    }()
+
+    definition.units = try {
+        var units = [String: String]()
+
+        try NSRegularExpression.find("case.+", string: def).forEach { match in
+            let trim = match.remove("case").remove(" ").split("=")
+
+            units[trim.first ?? ""] = trim.last ?? ""
+        }
+
+        return units
+    }()
+
+
+    definition.defaultScale = try {
+        guard let match = try NSRegularExpression.find("\\w+.rawValue", string: def).first else {
+            return ""
+        }
+
+        return match.split(".").first ?? ""
+    }()
 
     return definition
 }
@@ -97,4 +164,4 @@ func apply(template: String, definition: Definition) -> String {
 }
 
 // MAIN
-//try action()
+try action()
